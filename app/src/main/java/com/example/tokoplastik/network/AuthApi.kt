5d18e8f7d930ca66a68c757ce8a27fcf96d932de
DESKTOP-P4LCL1B\BAGAS-PC <bagas.aja20155@gmail.com>
2024-10-19 12:47:43 +0700

    auth navigation, api setup

---
+++ app/src/main/java/com/example/tokoplastik/network/AuthApi.kt
@@ -0,0 +1,17 @@
+package com.example.tokoplastik.network
+
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface AuthApi {
+
+    @FormUrlEncoded
+    @POST("login")
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Any
+
+
+}
