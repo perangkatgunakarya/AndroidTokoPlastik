427dff1cc3eeed108e8c6bca7457d075b929be80
DESKTOP-P4LCL1B\BAGAS-PC <bagas.aja20155@gmail.com>
2024-10-28 11:06:08 +0700

    fix api product error 500

--- app/src/main/java/com/example/tokoplastik/ui/product/ProductFragment.kt
+++ app/src/main/java/com/example/tokoplastik/ui/product/ProductFragment.kt
@@ -1,13 +1,16 @@
 package com.example.tokoplastik.ui.product
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tokoplastik.adapter.ProductAdapter
+import com.example.tokoplastik.data.UserPreferences
 import com.example.tokoplastik.data.network.GetProductApi
 import com.example.tokoplastik.data.repository.ProductRepository
 import com.example.tokoplastik.data.responses.GetProduct
@@ -15,23 +18,26 @@ import com.example.tokoplastik.databinding.FragmentProductBinding
 import com.example.tokoplastik.ui.base.BaseFragment
 import com.example.tokoplastik.util.Resource
 import com.example.tokoplastik.util.handleApiError
+import com.example.tokoplastik.util.visible
 import com.example.tokoplastik.viewmodel.ProductViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 
 class ProductFragment : BaseFragment<ProductViewModel, FragmentProductBinding, ProductRepository> () {
 
-    private lateinit var adapter: ProductAdapter
     private lateinit var getProduct: List<GetProduct>
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         getProduct = listOf()
 
+        viewModel.getProduct()
         viewModel.product.observe(viewLifecycleOwner, Observer {
-            when(it) {
+            when (it) {
                 is Resource.Success -> {
                     lifecycleScope.launch {
                         withContext (Dispatchers.Main) {
@@ -40,9 +46,6 @@ class ProductFragment : BaseFragment<ProductViewModel, FragmentProductBinding, P
                                 layoutManager = LinearLayoutManager(requireContext())
                             }
                         }
-                        //
-                        val product = getProduct
-                        Toast.makeText(requireContext(), product.toString(), Toast.LENGTH_LONG).show()
                     }
                 }
                 is Resource.Failure -> {
@@ -61,5 +64,9 @@ class ProductFragment : BaseFragment<ProductViewModel, FragmentProductBinding, P
         container: ViewGroup?
     ) = FragmentProductBinding.inflate(inflater, container, false)
 
-    override fun getFragmentRepository() = ProductRepository(remoteDataSource.buildApi(GetProductApi::class.java))
+    override fun getFragmentRepository() : ProductRepository {
+        val token = runBlocking { userPreferences.authToken.first() }
+        val api = remoteDataSource.buildApi(GetProductApi::class.java, token)
+        return ProductRepository(api)
+    }
 }
