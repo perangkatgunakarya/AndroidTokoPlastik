2730c851c5bc1bb8b081b0114b84426e02a0bd37
DESKTOP-P4LCL1B\BAGAS-PC <bagas.aja20155@gmail.com>
2024-11-20 16:14:08 +0700

    new format of receipt

--- app/src/main/java/com/example/tokoplastik/util/ReceiptGenerator.kt
+++ app/src/main/java/com/example/tokoplastik/util/ReceiptGenerator.kt
@@ -20,55 +20,127 @@ import java.io.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-class ReceiptGenerator (
+class ReceiptGenerator(
     private val context: Context
-){
-    fun generatedPdfReceipt (
+) {
+    fun generatedPdfReceipt(
         orderData: Transaction,
         cartItems: List<CartItem>,
         orderId: String
-    ) : File {
+    ): File {
         val filename = "Invoice_${orderId}.pdf"
         val file = File(context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), filename)
 
-        val document = Document()
+        val document = Document(PageSize.A4)
         try {
             PdfWriter.getInstance(document, FileOutputStream(file))
             document.open()
 
-            // header
-            document.add(Paragraph("TOKO PLASTIK HAJI ALI", FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18f)))
-            document.add(Paragraph("INVOICE", FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18f)))
-            document.add(Paragraph("Tanggal: ${SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(Date())}", FontFactory.getFont(FontFactory.HELVETICA, 12f)))
-            document.add(Paragraph("No Invoice: TPHA-$orderId", FontFactory.getFont(FontFactory.COURIER, 12f)))
-            document.add(Paragraph("Customer: ${orderData.customerId}", FontFactory.getFont(FontFactory.COURIER, 12f)))
+            val blueHeader = BaseColor(51, 122, 183)
+            val grayText = BaseColor(108, 117, 125)
+            val alternateRow = BaseColor(249, 249, 249)
 
-            // table
+            val companyName = Paragraph(
+                "Toko Plastik H. Ali",
+                FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18f)
+            )
+            companyName.alignment = Element.ALIGN_LEFT
+            document.add(companyName)
+
+            val invoice = Paragraph(
+                "INVOICE",
+                FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18f, blueHeader)
+            )
+            invoice.alignment = Element.ALIGN_RIGHT
+            document.add(invoice)
+
+            val headerTable = PdfPTable(2)
+            headerTable.widthPercentage = 100f
+            headerTable.setWidths(floatArrayOf(1f, 1f))
+
+            val customerInfoCell = PdfPCell().apply {
+                border = Rectangle.NO_BORDER
+                val customerFont = FontFactory.getFont(FontFactory.HELVETICA, 12f, grayText)
+                addElement(Paragraph(orderData.customer.name, customerFont))
+                addElement(Paragraph(orderData.customer.address, customerFont))
+            }
+
+            val invoiceDetailsCell = PdfPCell().apply {
+                border = Rectangle.NO_BORDER
+                horizontalAlignment = Element.ALIGN_RIGHT
+                val detailsFont = FontFactory.getFont(FontFactory.HELVETICA, 11f)
+                addElement(Paragraph("Referensi    : TPHA-${orderId}", detailsFont))
+                addElement(Paragraph("Tanggal      : ${SimpleDateFormat("dd MMMM yyyy", Locale.getDefault()).format(Date())}", detailsFont))
+                addElement(Paragraph("Status       : ${orderData.paymentStatus}", detailsFont))
+            }
+
+            headerTable.addCell(customerInfoCell)
+            headerTable.addCell(invoiceDetailsCell)
+
+            document.add(Paragraph("\n"))
+            document.add(headerTable)
+            document.add(Paragraph("\n"))
+
+            document.add(Paragraph("\n"))
+
             val table = PdfPTable(5)
             table.widthPercentage = 100f
-            table.setWidths(floatArrayOf(3f, 1f, 1f, 1f, 1f))
+            table.setWidths(floatArrayOf(2.5f, 1f, 1.5f, 1f, 1.5f))
 
-            // table headers
-            arrayOf("Product", "Price", "Quantity", "Unit", "Total").forEach { header ->
-                val cell = PdfPCell(Phrase(header))
+            val headerFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 11f, BaseColor.WHITE)
+            arrayOf("Produk", "Kuantitas", "Harga", "Unit", "Jumlah").forEach { header ->
+                val cell = PdfPCell(Phrase(header, headerFont))
+                cell.backgroundColor = blueHeader
+                cell.setPadding(8f)
                 cell.horizontalAlignment = Element.ALIGN_CENTER
+                cell.verticalAlignment = Element.ALIGN_MIDDLE
                 table.addCell(cell)
             }
 
-            cartItems.forEach { cartItem ->
-                table.addCell(cartItem.productPrice.first().product.name)
-                table.addCell(String.format("Rp. %,d", cartItem.customPrice))
-                table.addCell(cartItem.quantity.toString())
-                table.addCell(cartItem.selectedPrice.unit)
-                table.addCell(String.format("Rp. %,d", cartItem.customPrice * cartItem.quantity))
+            val contentFont = FontFactory.getFont(FontFactory.HELVETICA, 10f)
+            var isAlternateRow = false
+
+            cartItems.forEach { item ->
+                val cells = arrayOf(
+                    item.product?.data?.name,
+                    item.quantity.toString(),
+                    String.format("Rp %,d", item.customPrice),
+                    item.selectedPrice.unit,
+                    String.format("Rp %,d", item.customPrice * item.quantity)
+                )
+
+                cells.forEach { content ->
+                    val cell = PdfPCell(Phrase(content, contentFont))
+                    if (isAlternateRow) {
+                        cell.backgroundColor = alternateRow
+                    }
+                    cell.setPadding(8f)
+                    cell.horizontalAlignment = Element.ALIGN_CENTER
+                    table.addCell(cell)
+                }
+                isAlternateRow = !isAlternateRow
             }
 
             document.add(table)
             document.add(Paragraph("\n"))
 
-            // footer
-            document.add(Paragraph("Total: Rp. ${String.format("Rp. %,d", orderData.total)}", FontFactory.getFont(FontFactory.COURIER_BOLD, 12f)))
-            document.add(Paragraph("Status Pembayaran: ${orderData.paymentStatus}", FontFactory.getFont(FontFactory.COURIER_BOLD, 12f)))
+            val totalFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 11f)
+
+            val totalPara = Paragraph(
+                "Total: ${String.format("Rp %,d", orderData.total.toLong())}",
+                FontFactory.getFont(FontFactory.HELVETICA_BOLD, 12f, blueHeader)
+            )
+            totalPara.alignment = Element.ALIGN_RIGHT
+            document.add(totalPara)
+
+            document.add(Paragraph("\n\n"))
+
+            val footer = Paragraph(
+                "Dengan Hormat,\n\n\n\nToko Plastik H. Ali\nH. Ali",
+                FontFactory.getFont(FontFactory.HELVETICA, 11f)
+            )
+            footer.alignment = Element.ALIGN_CENTER
+            document.add(footer)
 
         } catch (e: Exception) {
             e.printStackTrace()
@@ -101,31 +173,67 @@ class ReceiptGenerator (
         cartItems: List<CartItem>,
         orderId: String
     ) {
-
         val printerCommands = StringBuilder()
-        // initialize printer
-        printerCommands.append(byteArrayOf(0x1B, 0x40).toString())
-        // set line spacing
-        printerCommands.append(byteArrayOf(0x1B, 0x33, 0x18).toString())
 
-        //header
-        printerCommands.append("TOKO PLASTIK HAJI ALI\n")
+        printerCommands.append(byteArrayOf(0x1B, 0x40).toString()) // Initialize printer
+        printerCommands.append(byteArrayOf(0x1B, 0x33, 0x18).toString()) // Set line spacing
+        printerCommands.append(byteArrayOf(0x1B, 0x61, 0x01).toString()) // Center alignment
+
+        printerCommands.append("PT PERANGKAT GUNA KARYA\n")
+        printerCommands.append(byteArrayOf(0x1B, 0x45, 0x01).toString()) // Bold ON
         printerCommands.append("INVOICE\n")
-        printerCommands.append("Tanggal: ${SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(Date())}\n")
-        printerCommands.append("No Invoice: TPHA-${orderId}\n")
-        printerCommands.append("Customer: ${orderData.customerId}")
+        printerCommands.append(byteArrayOf(0x1B, 0x45, 0x00).toString()) // Bold OFF
+        printerCommands.append(byteArrayOf(0x1B, 0x61, 0x00).toString()) // Left alignment
+
+        printerCommands.append("\n")
+        printerCommands.append("${orderData.customer.name}\n")
+        printerCommands.append("${orderData.customerId}\n")
+
+        printerCommands.append("\n")
+        printerCommands.append("Referensi   : INV-${orderId}\n")
+        printerCommands.append("Tanggal     : ${SimpleDateFormat("dd MMMM yyyy", Locale.getDefault()).format(Date())}\n")
+        printerCommands.append("STATUS      : ${orderData.paymentStatus}\n")
         printerCommands.append("\n")
 
-        // table
-        printerCommands.append(String.format("%-20s %8s %12s %12s %12s\n", "Product", "Price", "Quantity", "Unit", "Total"))
-        printerCommands.append("-".repeat(64) + "\n")
+        printerCommands.append("----------------------------------------\n")
+        printerCommands.append(
+            String.format(
+                "%-15s %-8s %-5s %-8s %8s\n",
+                "Produk", "Harga", "Qty", "Unit", "Jumlah"
+            )
+        )
+        printerCommands.append("----------------------------------------\n")
+
+        cartItems.forEach { item ->
+            printerCommands.append(String.format("%-15s\n", item.product?.data?.name))
+            printerCommands.append(
+                String.format(
+                    "%8.0f x %-5d %8.0f%% %11.0f\n",
+                    item.customPrice,
+                    item.quantity,
+                    item.selectedPrice.unit,
+                    item.customPrice * item.quantity
+                )
+            )
+        }
+
+        printerCommands.append(String.format("Total %33.0f\n", orderData.total))
+        printerCommands.append("----------------------------------------\n")
+
+        printerCommands.append("\n")
+        printerCommands.append(byteArrayOf(0x1B, 0x61, 0x01).toString()) // Center alignment
+        printerCommands.append("Dengan Hormat,\n\n\n")
+        printerCommands.append("Toko Plastik H. Ali\n")
+        printerCommands.append("Penjualan\n")
+
+        printerCommands.append("\n\n\n\n")
+        printerCommands.append(byteArrayOf(0x1D, 0x56, 0x41, 0x10).toString()) // Paper cut
 
         Thread {
             try {
                 val usbManager = context.getSystemService(Context.USB_SERVICE) as UsbManager
                 val deviceList = usbManager.deviceList
 
-                // Find Epson printer
                 val epsonDevice = deviceList.values.find { device ->
                     device.vendorId == 0x04b8 // Epson vendor ID
                 }
